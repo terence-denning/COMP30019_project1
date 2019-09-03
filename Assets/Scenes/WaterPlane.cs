98087738ba2047d5cf7cd87f85e7a4155f981fd7
Peng Yang Yu <pyu2@student.unimelb.edu.au>
2019-09-03 14:34:08 +1000

    Updated Water
    Water

---
+++ Assets/Scenes/WaterPlane.cs
@@ -0,0 +1,82 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class WaterPlane : MonoBehaviour
+{
+    public Shader shader;
+    public int xSize, zSize;
+    private Vector3[] vertices;
+    private Mesh mesh;
+    public Texture tex;
+    public float wavespeed;
+    Mesh  CreateMesh()
+    {
+        GetComponent<MeshFilter>().mesh = mesh;
+        mesh = new Mesh();
+        mesh.name = "WaterPlane";
+        int i = 0;
+        //Size of Plane;
+        vertices = new Vector3[(xSize + 1) * (zSize + 1)];
+        //Define UV;
+        Vector2[] uv = new Vector2[vertices.Length];
+        //tangent;
+        Vector4[] tangents = new Vector4[vertices.Length];
+        Vector4 tangent = new Vector4(1f, 0f, 0f, -1f);
+        //Define Vertex;
+        for (int y = 0; y <= zSize; y++)
+        {
+            for (int x = 0; x <= xSize; x++)
+            {
+                vertices[i] = new Vector3(x,0, y);
+                uv[i] = new Vector2((float)x / xSize,(float) y / zSize);
+                tangents[i] = tangent;
+                i++;
+            }
+        }
+        mesh.vertices = vertices;
+        //Define Triangle;
+
+        int[] triangles = new int[xSize * zSize* 6];
+        for (int t = 0, v = 0, y = 0; y < zSize; y++, v++)
+        {
+            for (int x = 0; x < xSize; x++, t += 6, v++)
+            {
+                triangles[t] = v;
+                triangles[t + 1] = v + xSize + 1;
+                triangles[t + 2] = v + 1;
+                triangles[t + 3] = v + 1;
+                triangles[t + 4] = v + xSize + 1;
+                triangles[t + 5] = v +  xSize + 2;
+            }
+        }
+        mesh.triangles = triangles;
+        mesh.RecalculateNormals();
+        mesh.uv = uv;
+        mesh.tangents = tangents;
+        
+        return mesh;
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        MeshFilter mesh = this.gameObject.AddComponent<MeshFilter>();
+        mesh.mesh = this.CreateMesh();
+        MeshRenderer renderer = this.gameObject.AddComponent<MeshRenderer>();
+       
+        renderer.material.shader = this.shader;
+        renderer.material.mainTexture = this.tex;
+        
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        MeshRenderer r = this.gameObject.GetComponent<MeshRenderer>();
+        MeshFilter m = this.gameObject.GetComponent<MeshFilter>();
+        Shader.SetGlobalFloat("WaveSpeed",this.wavespeed);
+       
+
+    }
+}
---
+++ Assets/Scenes/WaveShader.shader
@@ -0,0 +1,62 @@
+﻿//UNITY_SHADER_NO_UPGRADE
+
+Shader "Unlit/WaveShader"
+{
+    
+	Properties
+	{
+		_MainTex ("Texture", 2D) = "white" {}
+		WaveSpeed ("WaveSpeed", Float) = 2.0
+	}
+	SubShader
+	{
+		Pass
+		{
+			Cull Off
+
+			CGPROGRAM
+			#pragma vertex vert
+			#pragma fragment frag
+
+			#include "UnityCG.cginc"
+    
+			uniform sampler2D _MainTex;	
+			float WaveSpeed;
+            
+			struct vertIn
+			{
+				float4 vertex : POSITION;
+				float2 uv : TEXCOORD0;
+				
+			};
+
+			struct vertOut
+			{
+				float4 vertex : SV_POSITION;
+				float2 uv : TEXCOORD0;
+			};
+
+			// Implementation of the vertex shader
+			vertOut vert(vertIn v)
+			{
+				// Displace the original vertex in model space
+				float4 displacement = float4(0.0f,sin(_Time.y*(WaveSpeed)+v.vertex.x+v.vertex.z), 0.0f, 0.0f);
+				float2 uvdisplacement = float2(cos(_Time.y + v.uv.x)*0.1, sin(_Time.y+v.uv.y)*0.1);
+				vertOut o;
+				o.vertex = mul(UNITY_MATRIX_MVP, v.vertex);
+				o.vertex += displacement;
+				v.uv += uvdisplacement;
+				o.uv = v.uv;    
+				return o;
+			}
+			
+			// Implementation of the fragment shader
+			fixed4 frag(vertOut v) : SV_Target
+			{
+				fixed4 col = tex2D(_MainTex, v.uv);
+				return col;
+			}
+			ENDCG
+		}
+	}
+}
