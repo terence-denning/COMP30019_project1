eae0555b50427ba9996da64f2a458d4704fef316
Ricky <ricky@RickydeMacBook-Pro.local>
2019-09-11 15:01:58 +1000

    rotational sun

---
+++ Assets/Scenes/SunRotation.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SunRotation : MonoBehaviour
+{
+
+	[HideInInspector]
+	public GameObject sun;
+	[HideInInspector]
+	public Light sunLight;
+
+	[Range(0, 24)]
+	public float timeOfDay = 12;
+
+	public float secondsPerMinute = 60;
+	[HideInInspector]
+	public float secondsPerHour;
+	[HideInInspector]
+	public float secondsPerDay;
+
+	public float timeMultiplier = 1;
+
+	void Start()
+	{
+		sun = gameObject;
+		sunLight = gameObject.GetComponent<Light>();
+
+		secondsPerHour = secondsPerMinute * 60;
+		secondsPerDay = secondsPerHour * 24;
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		SunUpdate();
+
+		timeOfDay += (Time.deltaTime / secondsPerDay) * timeMultiplier;
+
+		if (timeOfDay >= 24)
+		{
+			timeOfDay = 0;
+		}
+	}
+
+	public void SunUpdate()
+	{
+		sun.transform.localRotation = Quaternion.Euler((timeOfDay / 24) * 360 - 0, 0.0f, 0.0f);
+	}
+}
